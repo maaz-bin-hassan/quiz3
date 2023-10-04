f59ac15f6afb3c51dba273b2d293f6f88acddd6d
maaz-bin-hassan <mleghari64@gmail.com>
2023-10-04 13:00:10 +0500

    added js file and DOM functon to print email pass to console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("signup-form");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        
+        console.log("Email: " + email);
+        console.log("Password: " + password);
+
+        // You can send the data to a server or perform further actions here
+    });
+});
